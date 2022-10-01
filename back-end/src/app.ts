a8a9ab10814cf410e7e0d34c435546a15bccbffb
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-01 11:29:16 -0300

    chore: correct import of error handler middleware in function of changing error handler middleware name

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,7 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import 'express-async-errors';
-import { errorHandlerMiddleware } from './middlewares/errorHanddlerMiddleware';
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
 
 dotenv.config();
 
