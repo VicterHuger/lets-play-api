814a1cb82121a4024a2a3e59ec4cb7c9fb2c30f5
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-30 21:58:06 -0300

    feat: add a server to listening the app in a PORT

---
+++ back-end/src/server.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT: number = +process.env.PORT || 5001;
+
+app.listen(PORT, (): void => console.log(`Server is listening on PORT ${PORT}`));
