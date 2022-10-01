b003c92dfbbfc7072f5087d2b1062752a71f75a4
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-01 11:17:02 -0300

    feat: add an error handler middleware to throw custmom errors in app

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,11 +1,13 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import 'express-async-errors';
+import { errorHandlerMiddleware } from './middlewares/errorHanddlerMiddleware';
 
 dotenv.config();
 
 const app: express.Express = express();
 
-app.use([cors(), json()]);
+app.use([cors(), json(), errorHandlerMiddleware]);
 
 export default app;
