ecc9161265e3f08f6ff16f574ce9142a89684d1c
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 15:23:03 -0300

    feat: add a route to login an user

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema";
-import { TypeUserSingUp } from "../types/userTypes";
+import { TypeUserSingUp, TypeUserSingIn } from "../types/userTypes";
 import signUpSchema from "../schemas/signUpSchema";
+import signInSchema from "../schemas/signInSchema";
 import { authController } from '../controllers/authController';
 
 
 const router: Router = Router();
 
 router.post('/sign-up', validateSchema<TypeUserSingUp>(signUpSchema), authController.createUser);
+
+router.post('/sign-in', validateSchema<TypeUserSingIn>(signInSchema), authController.singInUser);
 
 export default router;
