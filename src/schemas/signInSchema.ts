9f781f0be6582edeb427c49edbc60c84a8542e38
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 18:09:57 -0300

    refactor: use label information of each property of joi object to introduce in custom error messages

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -3,15 +3,15 @@ import { TypeUserSingIn } from '../types/userTypes';
 
 const signInSchema: joi.ObjectSchema<TypeUserSingIn> = joi.object({
     email: joi.string().email().required().label('Email').messages({
-        'string.base': 'Email must be a string',
-        'string.empty': 'Email field is not allowed to be empty',
-        'string.email': 'Email must be a valid email!',
-        'string.required': 'Email field is required!'
+        'string.base': '{#label} must be a string',
+        'string.empty': '{#label} field is not allowed to be empty',
+        'string.email': '{#label} must be a valid email!',
+        'string.required': '{#label} field is required!'
     }),
     password: joi.string().label('Password').required().messages({
-        'string.base': 'Password must be a string',
-        'string.empty': 'Password field is not allowed to be empty',
-        'string.required': 'Password field is required!'
+        'string.base': '{#label} must be a string',
+        'string.empty': '{#label} field is not allowed to be empty',
+        'string.required': '{#label} field is required!'
     }),
 })
 
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -3,23 +3,23 @@ import { TypeUserSingUp } from '../types/userTypes';
 
 const signUpSchema: joi.ObjectSchema<TypeUserSingUp> = joi.object({
     email: joi.string().email().required().label('Email').messages({
-        'string.base': 'Email must be a string',
-        'string.empty': 'Email field is not allowed to be empty',
-        'string.email': 'Email must be a valid email!',
-        'string.required': 'Email field is required!'
+        'string.base': '{#label} must be a string',
+        'string.empty': '{#label} field is not allowed to be empty',
+        'string.email': '{#label} must be a valid email!',
+        'string.required': '{#label} field is required!'
     }),
     password: joi.string().pattern(/(?=.*?[A-Z])/, 'upercase letter').pattern(/(?=.*?[a-z])/, 'Lowercase letter').pattern(/(?=.*?[0-9])/, 'number').pattern(/(?=.*?[#?!@$%^&*-])/, 'special character').min(6).required().messages({
-        'string.base': 'Password must be a string',
-        'string.empty': 'Password field is not allowed to be empty',
-        'string.required': 'Password field is required!',
-        'string.min': 'Password must contains at least {#limit} characteres',
-        'string.pattern.name': 'Passwrod must contains at least 1 {#name}'
+        'string.base': '{#label} must be a string',
+        'string.empty': '{#label} field is not allowed to be empty',
+        'string.required': '{#label} field is required!',
+        'string.min': '{#label} must contains at least {#limit} characteres',
+        'string.pattern.name': '{#label} must contains at least 1 {#name}'
     }),
     confirmPassword: joi.string().valid(joi.ref('password')).label('Confirm password').required().messages({
-        'string.base': 'Confirm password must be a string',
-        'string.empty': 'Confirm password field is not allowed to be empty',
-        'string.required': 'Confirm password field is required!',
-        'any.only': 'Confirm password field must be equal to password field'
+        'string.base': '{#label} must be a string',
+        'string.empty': '{#label} field is not allowed to be empty',
+        'string.required': '{#label} field is required!',
+        'any.only': '{#label} field must be equal to password field'
     }),
 })
 
