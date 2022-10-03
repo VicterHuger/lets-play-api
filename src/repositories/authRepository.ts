8d3c6cad523929718b26b152be1bf23b90397c78
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 14:53:52 -0300

    feat: add a function to create an user

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,9 +1,18 @@
 import prisma from '../config/database';
 
+async function createUser(email: string, password: string) {
+    return prisma.user.create({
+        data: {
+            email, password
+        }
+    });
+}
+
 async function getUserByEmail(email: string) {
     return prisma.user.findUnique({ where: { email } })
 }
 
 export const authRepository = {
-    getUserByEmail
+    getUserByEmail,
+    createUser
 }
