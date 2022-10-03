eae34f746c0aaaa3ff7d878dcce9fcaf1183837d
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 15:05:21 -0300

    feat: add controller rules to createUser function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
+import { TypeUserSingUp } from '../types/userTypes';
+import { authService } from '../services/authService';
 
 async function createUser(req: Request, res: Response) {
+    const { email, password }: TypeUserSingUp = res.locals.body;
+
+    const user = await authService.createUser(email, password);
+
+    return res.status(201).send(user);
 
 }
 
