d87b862909a807164dc1148464b4fc01c4714df5
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 12:41:55 -0300

    feat: import the routes of the app and use them in the app

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import 'express-async-errors';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
+import router from './routes/router';
 
 dotenv.config();
 
 const app: express.Express = express();
 
-app.use([cors(), json(), errorHandlerMiddleware]);
+app.use([cors(), json(), router, errorHandlerMiddleware]);
 
 export default app;
