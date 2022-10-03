bae88012e2be129bedf04083b462d83f233a24e5
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 17:28:45 -0300

    refactor: create custom error messages in joi.object definition

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -2,8 +2,17 @@ import joi from 'joi';
 import { TypeUserSingIn } from '../types/userTypes';
 
 const signInSchema: joi.ObjectSchema<TypeUserSingIn> = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+    email: joi.string().email().required().label('Email').messages({
+        'string.base': 'Email must be a string',
+        'string.empty': 'Email field is not allowed to be empty',
+        'string.email': 'Email must be a valid email!',
+        'string.required': 'Email field is required!'
+    }),
+    password: joi.string().label('Password').required().messages({
+        'string.base': 'Password must be a string',
+        'string.empty': 'Password field is not allowed to be empty',
+        'string.required': 'Password field is required!'
+    }),
 })
 
 export default signInSchema;
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,9 +2,25 @@ import joi from 'joi';
 import { TypeUserSingUp } from '../types/userTypes';
 
 const signUpSchema: joi.ObjectSchema<TypeUserSingUp> = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().pattern(/(?=.*?[A-Z])/).pattern(/(?=.*?[a-z])/).pattern(/(?=.*?[0-9])/).pattern(/(?=.*?[#?!@$%^&*-])/).min(8).required(),
-    confirmPassword: joi.string().valid(joi.ref('password')).required(),
+    email: joi.string().email().required().label('Email').messages({
+        'string.base': 'Email must be a string',
+        'string.empty': 'Email field is not allowed to be empty',
+        'string.email': 'Email must be a valid email!',
+        'string.required': 'Email field is required!'
+    }),
+    password: joi.string().pattern(/(?=.*?[A-Z])/, 'upercase letter').pattern(/(?=.*?[a-z])/, 'Lowercase letter').pattern(/(?=.*?[0-9])/, 'number').pattern(/(?=.*?[#?!@$%^&*-])/, 'special character').min(6).required().messages({
+        'string.base': 'Password must be a string',
+        'string.empty': 'Password field is not allowed to be empty',
+        'string.required': 'Password field is required!',
+        'string.min': 'Password must contains at least {#limit} characteres',
+        'string.pattern.name': 'Passwrod must contains at least 1 {#name}'
+    }),
+    confirmPassword: joi.string().valid(joi.ref('password')).label('Confirm password').required().messages({
+        'string.base': 'Confirm password must be a string',
+        'string.empty': 'Confirm password field is not allowed to be empty',
+        'string.required': 'Confirm password field is required!',
+        'any.only': 'Confirm password field must be equal to password field'
+    }),
 })
 
 export default signUpSchema;
