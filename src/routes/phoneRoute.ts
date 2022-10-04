f2e06dba2420ea0ba7b10942a8e1d484646cd28c
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 09:50:50 -0300

    feat: add a route to get a phone by its id

--- src/routes/phoneRoute.ts
+++ src/routes/phoneRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { phoneController } from '../controllers/phoneController';
 import { tokenValidation } from '../middlewares/tokenValidation';
+import { validateParamId } from '../middlewares/validateParam';
 import { validateSchema } from '../middlewares/validateSchema';
 import phoneSchema from '../schemas/phoneSchema';
 import { TypePhoneInsert } from '../types/phoneType';
@@ -10,5 +11,7 @@ const router: Router = Router();
 router.post('/phone/create', tokenValidation, validateSchema<TypePhoneInsert>(phoneSchema), phoneController.createPhone);
 
 router.get('/phone', tokenValidation, validateSchema<TypePhoneInsert>(phoneSchema), phoneController.getPhoneByNumber);
+
+router.get('/phone/:id', tokenValidation, validateParamId, phoneController.getPhoneById)
 
 export default router;
