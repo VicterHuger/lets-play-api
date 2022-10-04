ecb83b4789372585d23827892a9741124a4a2acb
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 01:07:59 -0300

    feat: add a controller function that receive the body with the link and send a response with the photo if it exist

--- src/controllers/photoController.ts
+++ src/controllers/photoController.ts
@@ -11,7 +11,16 @@ async function createPhoto(_req: Request, res: Response) {
     return res.status(201).send(photo);
 }
 
+async function getPhotoByLink(_req: Request, res: Response) {
+    const body: TypePhotoInsert = res.locals.body;
+
+    const photo: Photo = await photoService.getPhotoByLink(body.link);
+
+    return res.status(201).send(photo);
+
+}
 
 export const photoController = {
-    createPhoto
+    createPhoto,
+    getPhotoByLink
 }
