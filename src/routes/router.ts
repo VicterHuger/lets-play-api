5642c902e60efcd4a30f37ca94678382b93b4327
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 01:51:51 -0300

    feat: add phone router to routes of app

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import authRouter from './authRoute';
 import photoRouter from './photoRoute';
+import phoneRouter from './phoneRoute';
 
 const router: Router = Router();
 
-router.use([authRouter, photoRouter])
+router.use([authRouter, photoRouter, phoneRouter])
 
 export default router;
