0ae993db64d382eed1f093ec3c792eeb0771671c
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 23:30:02 -0300

    feat: add a controller to create a photo, sending status 201 and the photo created in case of successfull usage of the photo route

---
+++ src/controllers/photoController.ts
@@ -0,0 +1,17 @@
+import { Photo } from "@prisma/client";
+import { Request, Response } from "express";
+import { TypePhotoInsert } from '../types/photoType';
+import { photoService } from '../services/photoService';
+
+async function createPhoto(_req: Request, res: Response) {
+    const body: TypePhotoInsert = res.locals.body;
+
+    const photo: Photo = await photoService.createPhoto(body);
+
+    return res.status(201).send(photo);
+}
+
+
+export const photoController = {
+    createPhoto
+}
