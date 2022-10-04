b933aa2f19f6e9dae0604436628556ab95ce9543
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 01:43:32 -0300

    feat: add a route to create a phone

---
+++ src/routes/phoneRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { phoneController } from '../controllers/phoneController';
+import { tokenValidation } from '../middlewares/tokenValidation';
+import { validateSchema } from '../middlewares/validateSchema';
+import phoneSchema from '../schemas/phoneSchema';
+import { TypePhoneInsert } from '../types/phoneType';
+
+const router: Router = Router();
+
+router.post('/phone/create', tokenValidation, validateSchema<TypePhoneInsert>(phoneSchema), phoneController.createPhone);
+
+
+export default router;
