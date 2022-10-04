4a795a06000547276e58c8edb0b4fcf0848a85d7
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 01:08:53 -0300

    feat: add a route to get a photo with its link

--- src/routes/photoRoute.ts
+++ src/routes/photoRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { photoController } from '../controllers/photoController';
 import { tokenValidation } from '../middlewares/tokenValidation';
+import { validateParam } from '../middlewares/validateParam';
 import { validateSchema } from '../middlewares/validateSchema';
 import photoSchema from '../schemas/photoSchema';
 import { TypePhotoInsert } from '../types/photoType';
@@ -8,5 +9,7 @@ import { TypePhotoInsert } from '../types/photoType';
 const router: Router = Router();
 
 router.post('/photo/create', tokenValidation, validateSchema<TypePhotoInsert>(photoSchema), photoController.createPhoto);
+
+router.get('/photo', tokenValidation, validateSchema<TypePhotoInsert>(photoSchema), photoController.getPhotoByLink);
 
 export default router;
