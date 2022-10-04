409be724e0da120d7f35651f3d797c885add7927
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 23:27:10 -0300

    feat: add a create photo function to insert in database a new photo

--- src/repositories/photoRepository.ts
+++ src/repositories/photoRepository.ts
@@ -1,9 +1,17 @@
 import prisma from "../config/database";
+import { TypePhotoInsert } from "../types/photoType";
 
 async function findPhotoByLink(link: string) {
     return await prisma.photo.findUnique({ where: { link } })
 }
 
+async function createPhoto(photo: TypePhotoInsert) {
+    return await prisma.photo.create({
+        data: { ...photo }
+    })
+}
+
 export const photoRepository = {
-    findPhotoByLink
+    findPhotoByLink,
+    createPhoto
 }
