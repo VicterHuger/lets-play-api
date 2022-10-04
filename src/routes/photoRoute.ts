5b250a8a6a90a0d96f729c0055f364fed44e9e36
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-03 23:31:13 -0300

    feat: add a photo router to photo routes

---
+++ src/routes/photoRoute.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { photoController } from '../controllers/photoController';
+import { tokenValidation } from '../middlewares/tokenValidation';
+import { validateSchema } from '../middlewares/validateSchema';
+import photoSchema from '../schemas/photoSchema';
+import { TypePhotoInsert } from '../types/photoType';
+
+const router: Router = Router();
+
+router.post('/photo/create', tokenValidation, validateSchema<TypePhotoInsert>(photoSchema), photoController.createPhoto)
+
+export default router;
