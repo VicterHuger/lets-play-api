45ebc61dd0f6fd2e560167d3aa0f840f00072085
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-04 09:15:34 -0300

    style: add semicolon to end of statements

--- src/repositories/photoRepository.ts
+++ src/repositories/photoRepository.ts
@@ -2,14 +2,14 @@ import prisma from "../config/database";
 import { TypePhotoInsert } from "../types/photoType";
 
 async function findPhotoByLink(link: string) {
-    return await prisma.photo.findUnique({ where: { link } })
-}
+    return await prisma.photo.findUnique({ where: { link } });
+};
 
 async function createPhoto(photo: TypePhotoInsert) {
     return await prisma.photo.create({
         data: { ...photo }
-    })
-}
+    });
+};
 
 export const photoRepository = {
     findPhotoByLink,
