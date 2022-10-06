d06977a4ec949a6a196001008536546c3a7cb328
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-06 17:40:44 -0300

    feat(route): :sparkles: add sportRouter to router app

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,9 +4,10 @@ import addressRouter from './addressRoute';
 import photoRouter from './photoRoute';
 import phoneRouter from './phoneRoute';
 import profileRouter from './profileRoute';
+import sportRouter from './sportRoute';
 
 const router: Router = Router();
 
-router.use([authRouter, addressRouter, photoRouter, phoneRouter, profileRouter])
+router.use([authRouter, addressRouter, photoRouter, phoneRouter, profileRouter, sportRouter])
 
 export default router;
