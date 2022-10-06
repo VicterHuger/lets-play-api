a76e0bea8a9020152bdded2d53c11511dc856e4b
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 23:55:59 -0300

    refactor(route): :recycle: change variables imports and schema names

--- src/routes/addressRoute.ts
+++ src/routes/addressRoute.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { addressController } from "../controllers/addressController";
 import { tokenValidation } from "../middlewares/tokenValidation";
 import { validateSchema } from "../middlewares/validateSchema";
-import addressSchema from "../schemas/addressSchema";
-import { IAddressSchema } from "../types/addressType";
+import { addressSchemaCreate, AddressSchemaGet } from "../schemas/addressSchema";
+import { IAddressSchema, TypeAdressSchema } from "../types/addressType";
 
 const router: Router = Router();
 
-router.post('/address/create', tokenValidation, validateSchema<IAddressSchema>(addressSchema), addressController.createAddress);
+router.post('/address/create', tokenValidation, validateSchema<IAddressSchema>(addressSchemaCreate), addressController.createAddress);
 
 export default router;
