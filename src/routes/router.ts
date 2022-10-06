0e6c8095f786446020bbf5aa6ea28674ffd00e60
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 22:42:21 -0300

    refactor(route): add address route to app router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import authRouter from './authRoute';
+import addressRouter from './addressRoute';
 import photoRouter from './photoRoute';
 import phoneRouter from './phoneRoute';
 
 const router: Router = Router();
 
-router.use([authRouter, photoRouter, phoneRouter])
+router.use([authRouter, addressRouter, photoRouter, phoneRouter])
 
 export default router;
