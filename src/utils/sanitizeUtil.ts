44adac4a43d7d806a01506367aee45e666b5ba3b
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-06 16:02:40 -0300

    refactor: :recycle: add a transformation of object properties that includes id in its name to number type

--- src/utils/sanitizeUtil.ts
+++ src/utils/sanitizeUtil.ts
@@ -4,6 +4,10 @@ export function sanitizeObject<T>(object: T): void {
     for (const key of Object.keys(object)) {
         if (typeof (object[key]) === "string") {
             object[key] = stripHtml(object[key])?.result.trim() ?? object[key];
+            if (key?.toLowerCase()?.includes('id')) {
+                object[key] = Number(object[key]);
+            }
         }
+
     }
 }
