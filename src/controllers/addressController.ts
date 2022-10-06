669bf9af31e4e257a3a7ee4db5ae269fd20912ea
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 23:52:14 -0300

    refactor(type): :label: add the specific type of the body

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { addressService } from '../services/addressService';
 
 async function createAddress(_req: Request, res: Response) {
-    const body = res.locals.body;
+    const body: IAddressSchema = res.locals.body;
 
     const address = await addressService.createAddress(body);
 
