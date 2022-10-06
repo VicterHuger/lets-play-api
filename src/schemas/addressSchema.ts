45355207492972f5d7564f108dd63b59e479db93
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 23:49:32 -0300

    feat(schema): add a schema to get an address

--- src/schemas/addressSchema.ts
+++ src/schemas/addressSchema.ts
@@ -31,4 +31,21 @@ export const addressSchemaCreate: joi.ObjectSchema<IAddressSchema> = joi.object(
 
 });
 
-export default addressSchema;
+export const AddressSchemaGet: joi.ObjectSchema<TypeAdressSchema> = joi.object({
+    zipCode: joi.string().pattern(/^[0-9]+$/, 'numbers').length(8).required().label('Zipcode').messages({
+        'string.required': '{#label} must be provided!',
+        'string.empty': '{#label} must not be empty!',
+        'string.pattern.name': '{#label} must contains only {#name}',
+        'string.base': '{#label} must be a valid string',
+        'string.length': '{#label} must have {#limit} numbers'
+    }),
+    number: joi.string().pattern(/^[0-9]+$/, 'numbers').label('Number').messages({
+        'string.empty': '{#label} must not be empty!',
+        'string.pattern.name': '{#label} must contains only {#name}',
+        'string.base': '{#label} must be a valid string',
+    }),
+    complement: joi.string().label('Complement').messages({
+        'string.base': '{#label} must be a valid string',
+        'string.empty': '{#label} must not be empty!',
+    }),
+});
