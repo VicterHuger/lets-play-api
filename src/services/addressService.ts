de096912986a51de62b507aa855a25083d613842
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 22:37:29 -0300

    feat(service): :necktie: add a business logic to create an andress if the address doesn't exist and zipcode information lib doesn't be enough

---
+++ src/services/addressService.ts
@@ -0,0 +1,90 @@
+import { Address } from '@prisma/client';
+import cep, { CEP } from 'cep-promise';
+import { addressRepository } from '../repositories/addressRepository';
+import { cityService } from '../services/cityService';
+import { stateService } from '../services/stateService';
+import { ICepPromiseSuccessfull, IAddressInsert, IAddressSchema } from '../types/addressType';
+import { generateThrowErrorMessage } from '../utils/errorUtil';
+
+
+async function createAddress(partialAddress: IAddressSchema) {
+
+    const addressDataRaw = await getAddressInfoByZipCode(partialAddress.zipCode);
+
+    const addressData = await changeUndefinedFieldsAddress(partialAddress, addressDataRaw);
+
+    const addressDb = await addressRepository.getAddressByZipCodeNumberComplement(partialAddress.zipCode, partialAddress.number, partialAddress.complement);
+
+    if (!!addressDb) generateThrowErrorMessage('Conflict', 'This address was already registered');
+
+    const stateId = await getStateId(addressData.state);
+    const cityId = await getCityId(addressData.city, stateId);
+
+    const addressToBeInsert: IAddressInsert = constructObjectAddress(addressData.street, partialAddress.zipCode, addressData.neighborhood, cityId, partialAddress.complement, partialAddress.number);
+
+    const address: Address | null = await addressRepository.createAddress(addressToBeInsert);
+
+    if (!address) generateThrowErrorMessage('InternalServerError', 'Something went wrong and it was not possible to insert the new address');
+
+    return address;
+}
+
+async function getAddressInfoByZipCode(code: string): Promise<ICepPromiseSuccessfull | never> {
+    try {
+        const address: CEP = await cep(code);
+
+        delete address.service;
+
+        return address;
+
+    } catch (err) {
+        generateThrowErrorMessage('UnprocessableEntity', 'Invalid zipcode!');
+    }
+}
+
+async function changeUndefinedFieldsAddress(dataAddressUser: IAddressSchema, cepData: ICepPromiseSuccessfull) {
+    const result = { ...cepData }
+    for (const key of Object.keys(cepData)) {
+        if (cepData[key] === undefined) {
+            result[key] = dataAddressUser[key];
+        }
+    }
+    return result;
+}
+
+async function getStateId(uf: string) {
+    const state = await stateService.getStateByUf(uf);
+    return state.id;
+}
+
+async function getCityId(name: string, stateId: number) {
+    const city = await cityService.getCityByNameAndStateId(name, stateId);
+    return city.id;
+}
+
+
+function constructObjectAddress(street: string, zipCode: string, neighborhood: string, cityId: number, complement?: string, number?: string) {
+
+    const address = {
+        street,
+        number,
+        complement,
+        zipCode,
+        neighborhood,
+        cityId,
+    }
+
+    for (const key of Object.keys(address)) {
+        if (address[key] === undefined) {
+            delete address[key];
+        }
+    };
+
+    return address;
+
+}
+
+
+export const addressService = {
+    createAddress
+}
