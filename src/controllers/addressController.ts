9fae1628714daecdbf798e40c991c0f0960ecd20
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 23:53:12 -0300

    feat(controller): :sparkles: introduce a controller that returns status 200 and the address searched if all the requirements be fullfilled

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { addressService } from '../services/addressService';
+import { IAddressSchema, TypeAdressSchema } from "../types/addressType";
 
 async function createAddress(_req: Request, res: Response) {
     const body: IAddressSchema = res.locals.body;
@@ -9,6 +10,14 @@ async function createAddress(_req: Request, res: Response) {
     return res.status(201).send(address);
 }
 
+async function getAddress(_req: Request, res: Response) {
+    const body: TypeAdressSchema = res.locals.body;
+
+    const address = await addressService.getAddress(body);
+
+    return res.status(200).send(address);
+}
 export const addressController = {
-    createAddress
+    createAddress,
+    getAddress
 }
