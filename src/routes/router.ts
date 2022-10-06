63474651cbd8c784d1d4247c85cc091e522c06e9
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-06 16:17:25 -0300

    feat(route): :sparkles: add to router app the routes to update and insert profile

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,9 +3,10 @@ import authRouter from './authRoute';
 import addressRouter from './addressRoute';
 import photoRouter from './photoRoute';
 import phoneRouter from './phoneRoute';
+import profileRouter from './profileRoute';
 
 const router: Router = Router();
 
-router.use([authRouter, addressRouter, photoRouter, phoneRouter])
+router.use([authRouter, addressRouter, photoRouter, phoneRouter, profileRouter])
 
 export default router;
