38467e86cbada00caf9c7b745965a03490ac4dbb
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 22:22:07 -0300

    feat(schema): add a chema to validate address body information to include in database

---
+++ src/schemas/addressSchema.ts
@@ -0,0 +1,34 @@
+import joi from 'joi';
+import { IAddressSchema } from '../types/addressType';
+
+const addressSchema: joi.ObjectSchema<IAddressSchema> = joi.object({
+    zipCode: joi.string().pattern(/^[0-9]+$/, 'numbers').length(8).required().label('Zipcode').messages({
+        'string.required': '{#label} must be provided!',
+        'string.empty': '{#label} must not be empty!',
+        'string.pattern name': '{#label} must contains only {#name}',
+        'string.base': '{#label} must be a valid string',
+        'string.length': '{#label} must have {#limit} numbers'
+    }),
+    number: joi.string().pattern(/^[0-9]+$/, 'numbers').label('Number').messages({
+        'string.empty': '{#label} must not be empty!',
+        'string.pattern name': '{#label} must contains only {#name}',
+        'string.base': '{#label} must be a valid string',
+    }),
+    complement: joi.string().label('Complement').messages({
+        'string.base': '{#label} must be a valid string',
+        'string.empty': '{#label} must not be empty!',
+    }),
+    neighborhood: joi.string().required().label('Neighborhood').messages({
+        'string.base': '{#label} must be a valid string',
+        'string.empty': '{#label} must not be empty!',
+        'string.required': '{#label} must be provided!'
+    }),
+    street: joi.string().required().label('Street').messages({
+        'string.base': '{#label} must be a valid string',
+        'string.empty': '{#label} must not be empty!',
+        'string.required': '{#label} must be provided!'
+    })
+
+});
+
+export default addressSchema;
