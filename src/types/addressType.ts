25efeea429e02257243473ecdf9b579a5e7d6510
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 22:05:54 -0300

    feat(type): :label: add an interface to the successfull result of cep-promise library

--- src/types/addressType.ts
+++ src/types/addressType.ts
@@ -1,3 +1,11 @@
 import { Address } from "@prisma/client";
 
 export type TypeAdressSchema = Omit<Address, "id" | "street" | "neighborhood" | "cityId" | "createdAt" | "updatedAt">;
+
+export interface ICepPromiseSuccessfull {
+    cep: string,
+    state: string,
+    city: string,
+    street: string,
+    neighborhood: string,
+}
