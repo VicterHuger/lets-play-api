cd7d50d17b0029955b8c230b24d6585a5892a599
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-07 15:39:56 -0300

    feat: :sparkles: add a function to create a lobby into database

--- src/repositories/lobbyRepository.ts
+++ src/repositories/lobbyRepository.ts
@@ -1,4 +1,6 @@
 import prisma from "../config/database";
+import { TypeLobbyInsert } from "../types/lobbyType";
+
 async function findLobbyByUserHostId_eventDate_eventTimeStart_eventLocalId_minParticipants(userHostId: number, eventDate: Date, eventTimeStart: string, eventLocalId: number, minParticipants: number) {
     return await prisma.lobby.findUnique({
         where: {
@@ -9,6 +11,25 @@ async function findLobbyByUserHostId_eventDate_eventTimeStart_eventLocalId_minPa
     })
 }
 
+async function createLobby(lobbyInfo: TypeLobbyInsert, userId: number) {
+    return await prisma.lobby.create({
+        data: {
+            userHostId: userId,
+            ...lobbyInfo,
+            lobbiesUsers: {
+                create: {
+                    userId
+                }
+            }
+        },
+        include: {
+            lobbiesUsers: true
+        }
+
+    })
+}
+
 export const lobbyRepository = {
     findLobbyByUserHostId_eventDate_eventTimeStart_eventLocalId_minParticipants,
+    createLobby
 }
