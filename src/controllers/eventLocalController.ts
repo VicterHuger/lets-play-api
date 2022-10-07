7adbaf02aaa12de76926493ca443ef57fc61052a
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-07 15:33:39 -0300

    feat(controller): :sparkles: add controller functions that return status 200  and the event local

--- src/controllers/eventLocalController.ts
+++ src/controllers/eventLocalController.ts
@@ -11,6 +11,24 @@ async function createEventLocal(_req: Request, res: Response) {
     return res.status(201).send(eventLocal);
 }
 
+async function getEventLocalByAddressId(_req: Request, res: Response) {
+    const { addressId }: { addressId: number } = res.locals.param;
+
+    const eventLocal: EventLocal = await eventLocalService.getEventLocalByAddressId(addressId);
+
+    return res.status(200).send(eventLocal);
+}
+
+async function getEventLocalById(_req: Request, res: Response) {
+    const id: number = res.locals.id;
+
+    const eventLocal: EventLocal = await eventLocalService.getEventLocalById(id);
+
+    return res.status(200).send(eventLocal);
+}
+
 export const eventLocalController = {
     createEventLocal,
+    getEventLocalByAddressId,
+    getEventLocalById
 }
