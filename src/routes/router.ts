28bf7e077efea754251bbb03e02103067cf8c07d
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-10 11:00:07 -0300

    feat(route): :sparkles: modify app router to include the router to register an user in a lobby

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,6 +3,7 @@ import authRouter from './authRoute';
 import addressRouter from './addressRoute';
 import eventLocalRouter from './eventLocalRoute';
 import lobbyRouter from './lobbyRoute';
+import lobbyUserRouter from './lobbyUserRoute';
 import photoRouter from './photoRoute';
 import phoneRouter from './phoneRoute';
 import profileRouter from './profileRoute';
@@ -10,6 +11,6 @@ import sportRouter from './sportRoute';
 
 const router: Router = Router();
 
-router.use([authRouter, addressRouter, eventLocalRouter, lobbyRouter, photoRouter, phoneRouter, profileRouter, sportRouter])
+router.use([authRouter, addressRouter, eventLocalRouter, lobbyRouter, lobbyUserRouter, photoRouter, phoneRouter, profileRouter, sportRouter])
 
 export default router;
