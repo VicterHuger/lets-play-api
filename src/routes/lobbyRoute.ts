167aa241fb86aa5d509e60dc100d1a1d35efd372
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-10 11:56:46 -0300

    feat(route): :sparkles: add a GET /lobby:id to find a lobby with a specified id

--- src/routes/lobbyRoute.ts
+++ src/routes/lobbyRoute.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { lobbyController } from "../controllers/lobbyController";
 import { tokenValidation } from "../middlewares/tokenValidation";
+import { validateParamId } from "../middlewares/validateParam";
 import { validateSchema } from "../middlewares/validateSchema";
 import { lobbyInsertSchema } from "../schemas/lobbySchema";
 import { TypeLobbyInsert } from "../types/lobbyType";
 
 const router: Router = Router();
 
-router.post('/lobby/create', tokenValidation, validateSchema<TypeLobbyInsert>(lobbyInsertSchema), lobbyController.createLobby)
+router.post('/lobby/create', tokenValidation, validateSchema<TypeLobbyInsert>(lobbyInsertSchema), lobbyController.createLobby);
+
+router.get('/lobby/:id', tokenValidation, validateParamId, lobbyController.getLobbyById)
 
 export default router;
